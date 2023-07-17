00fc6f7cc61cc187ea08969b34bb67738f2c394c
yunsehwan <sh10302@naver.com>
2023-07-17 23:30:21 +0900

    [setting] 누락된 화면 추가

---
+++ presentation/mypage/src/main/java/com/strayalpaca/mypage/EditProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.strayalpaca.mypage
+
+import com.strayalpaca.mypage.databinding.EditProfileScreenBinding
+import com.strayalpaca.presentation.ViewBindingFragment
+
+class EditProfileFragment : ViewBindingFragment<EditProfileScreenBinding>(EditProfileScreenBinding::bind, R.layout.edit_profile_screen) {
+
+}
---
+++ presentation/mypage/src/main/java/com/strayalpaca/mypage/MypageFragment.kt
@@ -0,0 +1,8 @@
+package com.strayalpaca.mypage
+
+import com.strayalpaca.mypage.databinding.MypageScreenBinding
+import com.strayalpaca.presentation.ViewBindingFragment
+
+class MypageFragment : ViewBindingFragment<MypageScreenBinding>(MypageScreenBinding::bind, R.layout.mypage_screen) {
+
+}
